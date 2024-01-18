f9a9c5009bdf1432f5fc5ec7c884e0f5e5dd2668
flowermariya <flowermariya811@gmail.com>
2024-01-18 15:06:52 +0530

    FIX: Databse config changes

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,8 @@ import { AuthModule } from './auth/auth.module';
 import { ProductModule } from './product/product.module';
 import { CustomerModule } from './customer/customer.module';
 import { SalesModule } from './sales/sales.module';
+var fs = require('fs');
+import * as path from 'path';
 require('dotenv').config();
 
 @Module({
@@ -12,11 +14,15 @@ require('dotenv').config();
     AdminModule,
     TypeOrmModule.forRoot({
       type: 'mysql',
+      driver: require('mysql2'),
       host: process.env.DB_HOST,
       port: parseInt(process.env.DB_PORT),
       username: process.env.DB_USERNAME,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_DATABASE,
+      ssl: {
+        ca: fs.readFileSync(process.env.SSL_CA_PATH),
+      },
       entities: [__dirname + '/**/*.entity{.ts,.js}'],
       synchronize: process.env.DB_SYNC === 'true',
     }),
